01c2e51a7cda2ce9fd217deb7765194cf0d73e67
Markys97 <70927348+Markys97@users.noreply.github.com>
2022-02-27 18:41:44 +0300

    two section

--- js/slider.js
+++ js/slider.js
@@ -10,18 +10,21 @@ $('.program__slider').slick({
           breakpoint: 1200,
           settings: {
             slidesToShow: 3,
+            slidesToScroll: 2,
          }
         },
         {
           breakpoint: 992,
           settings: {
             slidesToShow: 2,
+            slidesToScroll: 1,
          }
         },
         {
           breakpoint: 576,
           settings: {
             slidesToShow: 1,
+            slidesToScroll: 1,
             centerMode: false,
          }
         }
