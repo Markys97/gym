c0955f43b9e21a2339c62715115b5b9bb0e92a42
Markys97 <70927348+Markys97@users.noreply.github.com>
2022-03-01 06:03:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 let faqItemImgs= document.querySelectorAll('.faq__item-img');
 let faqItemTexts= document.querySelectorAll('.faq__item-text');
+let burgerBtn= document.querySelector('.header__burger');
+let btnCloseNav= document.querySelector('.header__close-menu');
+let mobileNav=document.querySelector('.header__mobile');
+let body = document.querySelector('body');
+console.log(body)
+
+
+burgerBtn.addEventListener('click',e=>{
+    mobileNav.classList.add('open-mobile')
+    burgerBtn.style.display='none';
+    btnCloseNav.style.display='block';
+    body.classList.add('close')
+    
+})
+
+btnCloseNav.addEventListener('click',e=>{
+    mobileNav.classList.remove('open-mobile')
+    
+    burgerBtn.style.display='block';
+    btnCloseNav.style.display='none';
+
+    body.classList.remove('close')
+})
+
 
 
 let imgSrc;
@@ -7,6 +31,7 @@ let imgSrc;
 faqItemImgs.forEach(faqItemImg=>{
     faqItemImg.addEventListener('click',e=>{
         imgSrc= e.target.src;
+        console.log(imgSrc)
         
       faqItemTexts.forEach(faqItemText =>{
           if(faqItemImg.dataset.id === faqItemText.dataset.id){
@@ -14,10 +39,12 @@ faqItemImgs.forEach(faqItemImg=>{
               
               e.target.src= e.target.dataset.close
                  if(textHeight === '0px'){
+                     e.target.src=faqItemImg.dataset.close
                       faqItemText.classList.add('show-text')
                   }else{
                    faqItemText.classList.remove('show-text');
-                   e.target.src= imgSrc
+                   faqItemImg.setAttribute('src', imgSrc) 
+                   console.log(imgSrc)
                   }
           }
       })
